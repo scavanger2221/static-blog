6e3421079c8e879839cade978e07a4b337906b5a
dwi atmika w <dwi.atmikaw@gmail.com>
2023-09-14 17:24:13 +0800

    initial

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,9 +14,31 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+
+  const header = (
+    <header>
+      <div>
+        <h1>Static Blog</h1>
+        <p>Welcome to the static blog</p>
+      </div>
+    </header>
+  );
+
+  const footer = (
+    <footer>
+      <div>
+        <p>Developed by pepega</p>
+      </div>
+    </footer>
+  );
+
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en"> 
+      <body className={inter.className}>
+        {header}
+        {children}
+        {footer}
+        </body>
     </html>
   )
 }
---
+++ src/app/posts/[slug]/page.tsx
@@ -0,0 +1,11 @@
+const PostPage = (porps: any) => {
+    return (
+        <p>
+            <h1>
+                this is a post
+            </h1>
+        </p>
+    )
+}
+
+export default PostPage
